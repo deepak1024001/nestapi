b254b584095858c27df2eb83a0b5305d08d190f0
Deepak Kumar Khatri <deepak.k@sphinxworldbiz.com>
2023-03-15 21:26:47 +0530

    mysql configmac

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,27 @@
 import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
+import { UserService } from './user.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) { }
+  constructor(
+    private readonly appService: AppService,
+    private readonly userService: UserService,
+  ) { }
+
+  @Get('create-user')
+  async createUser() {
+    await this.userService.createUser({
+      name: `User anem ${Math.random()}`,
+      email: `useremail@${Math.random()}.com`,
+    });
+    return 'User created';
+  }
+
+  @Get('list')
+  userList() {
+    return this.userService.users({});
+  }
 
   @Get()
   getHello(): string {
@@ -17,7 +35,7 @@ export class AppController {
 
   @Get('build')
   getBuild() {
-    console.log(process.env)
-    return `Build Time: ${process.env.BUILD_TIME} Commit Hash: ${process.env.HASH} Version: ${process.env.VERSION} change`; 
+    console.log(process.env);
+    return `Build Time: ${process.env.BUILD_TIME} Commit Hash: ${process.env.HASH} Version: ${process.env.VERSION} change`;
   }
 }
