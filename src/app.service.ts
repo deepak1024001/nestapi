0ed7a1eef2628cac924c7faf2d6fac415015db45
Deepak Kumar Khatri <deepak.k@sphinxworldbiz.com>
2023-03-16 21:17:56 +0530

    Auto sync

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Wow nestjs is working';
+    return 'Wow nestjs is working last change';
   }
 }
